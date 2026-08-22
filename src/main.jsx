f1c6c4100373d94f88c82e1897d61837928b4ee8
Ferry Ayunda <ayundaferry919@gmail.com>
2026-08-23 05:50:23 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import NotFound from './pages/NotFound.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Halaman Utama / App */}
+        <Route path="/" element={<App />} />
+        
+        {/* Catch-all Route untuk Halaman 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
